bc0696dac6f849c1658690d503649f3b953c74fb
George Britto J <160612466+brittogeorge@users.noreply.github.com>
2024-12-17 11:50:41 +0530

    Add files via upload

---
+++ Components/index.js
@@ -0,0 +1,11 @@
+import Footer from "./Footer";
+import NavBar from "./NavBar";
+import Logo from "./Logo";
+import Menu from "./Menu";
+import Hero from "../Components/Hero";
+import PopUp from "../Components/PopUp";
+import Card from "../Components/Card";
+import Mn from "./Mn";
+import Det from "./Det";
+
+export {Footer, NavBar, Logo, Mn, Menu , PopUp, Card, Hero, Det };
